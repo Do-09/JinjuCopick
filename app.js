df2ec36e957d83a9335da27e7d06f070e3b6e470
Do-09 <ehgus5792@naver.com>
2023-04-05 04:56:43 +0900

    DB연결

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express(); 
+const bodyParser = require('body-parser');
 const ejs = require('ejs')
 
 app.set('view engine', 'ejs');
@@ -8,7 +9,10 @@ app.use('/public', express.static(__dirname + '/public'));
 
 const mainRouter = require('./router/mainRouter')
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 app.use("/", mainRouter)
+
 
 app.listen(5000, function(req,res){
     console.log("서버 실행");
--- router/mainRouter.js
+++ router/mainRouter.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router();
+const bodyParser = require('body-parser');
 const db = require('../model/db');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended:false}));
 
 router.get("/", function(req,res){
     res.render('main')
@@ -10,9 +14,29 @@ router.get("/login", function(req,res){
     res.render('login')
 })
 
-router.get("/join", function(req,res){
-    res.render('join')
-})
+router.post('/main', function(req, res) {
+    var id = req.body.id;
+    var password = req.body.password;
+    if (id && password) {             // id와 pw가 입력되었는지 확인
+        db.query('SELECT * FROM login WHERE id = ? AND password = ?', [id, password], function(error, results, fields) {
+            if (error) throw error;
+            if (results.length > 0) {       // db에서의 반환값이 있으면 로그인 성공
+                // req.session.is_logined = true;      // 세션 정보 갱신
+                // req.session.save(function () {
+                //     res.redirect(`/main`);
+                // });
+                res.redirect('/main');
+            } else {              
+                res.send(`<script type="text/javascript">alert("로그인 정보가 일치하지 않습니다."); 
+                document.location.href="/login";</script>`);    
+            }            
+        });
+
+    } else {
+        res.send(`<script type="text/javascript">alert("아이디와 비밀번호를 입력하세요!"); 
+        document.location.href="/login";</script>`);    
+    }
+});
 
 router.get("/join", function(req,res){
     res.render('join')
