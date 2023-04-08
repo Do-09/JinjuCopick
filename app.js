e4d7e318409b2595f47bdf69fe18df9877d2a846
Do-09 <ehgus5792@naver.com>
2023-04-08 14:40:44 +0900

    오류수정

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express(); 
 const ejs = require('ejs')
-const session = require('express-session')
-const FileStore = require('session-file-store')(session)
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -10,13 +8,6 @@ app.set('views', './views');
 app.use('/public', express.static(__dirname + '/public'));
 
 const mainRouter = require('./router/mainRouter')
-
-app.use(session({
-    secret: "key",
-    resave: false,
-    saveUninitialized:true,
-    store:new FileStore(),
-}))
 
 app.use("/", mainRouter)
 
--- router/mainRouter.js
+++ router/mainRouter.js
@@ -84,7 +84,7 @@ router.post("/signup/result", function(req,res){
                     res.send(`<script type="text/javascript">alert("회원가입이 완료되었습니다!");
                     document.location.href="/";</script>`);
                 });
-            } else if (password != password2) {                     // 비밀번호가 올바르게 입력되지 않은 경우
+            } else if (password1 != password2) {                     // 비밀번호가 올바르게 입력되지 않은 경우
                 res.send(`<script type="text/javascript">alert("입력된 비밀번호가 서로 다릅니다."); 
                 document.location.href="/signup";</script>`);    
             }
